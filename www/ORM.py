b409b60d2b972dea6aa5dd7318ac5c66108a3d8a
Wangbj <wangbj@lierda.com>
2020-06-15 15:41:41 +0800

    add ORM.py

--- www/ORM.py
+++ www/ORM.py
@@ -57,5 +57,74 @@ async def execute(sql, args, autocommit=True):
         return affected
 
 
+def create_args_string(num):
+    L = []
+    for n in range(num):
+        L.append('?')
+        return '.'.join(L)
 
 
+class Field(object):
+
+    def __init__(self, name, column_type, primary_key, default):
+        self.name = name
+        self.colume_type = column_type
+        self.primary_key = primary_key
+        self.default = default
+
+    def __str__(self):
+        return '<%s, %s:%s>' % (self.__class__.__neme__, self.colume_type, self.name)
+
+
+class StringField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+        super().__init__(name, ddl, primary_key, default)
+
+
+class BooleanField(Field):
+
+    def __init__(self, name=None, default=False):
+        super().__init__(name, 'boolean', False, default)
+
+
+class IntegerField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=0):
+        super().__init__(name, 'bigint', primary_key, default)
+
+
+class FloatField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=0.0):
+        super().__init__(name, 'real', primary_key, default)
+
+
+class TextField(Field):
+
+    def __init__(self, name=None, default=None):
+        super().__init__(name, 'text', False, default)
+
+
+class ModelMetaclass(type):
+
+    def __new__(cls, name, bases, attrs):
+        if name=='Model':
+            return type.__new__(cls, name, bases, attrs)
+        tableName = attrs.get('__table__', None) or name
+        logging.info('found model: %s (table: %s)' % (name, tableName))
+        mappings = dict()
+        fields = []
+        primaryKey = None
+        for k, v in attrs.items():
+            if isinstance(v, Field):
+                logging.info('    found mapping: %s ==> %s' % (k, v))
+                mappings[k] = v
+            if v.primary_key:
+                if primaryKey:
+                    raise BaseException('Duplicate primary key for field: %s' % k)
+                primaryKey  = k
+            else:
+                fields.append(k)
+        if not primaryKey:
+            raise BaseException('Primary key not found.')
